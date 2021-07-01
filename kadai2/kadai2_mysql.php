37b8d4ba2d9a7eaf70a83ebcad24b1e9937056c1
Yuta_Nakjima_Dell <nkjmyut0511@gmail.com>
2021-07-01 17:52:28 +0900

    データの挿入ができた．以下のサイトが分かりやすかった https://kinocolog.com/pdo_insert/

--- kadai2/kadai2_mysql.php
+++ kadai2/kadai2_mysql.php
@@ -19,12 +19,34 @@
         ) engine=innodb default charset=utf8';
         //$create = $dbh->query($createsql);
 
-        //データの取得
+
+    //データの挿入
+        $name = 'p1';
+        $comment = '最初のコメント';
+        $update_time = date("Y-m-d" ,time());
+        $pass = '123';
+
+        $sql = "INSERT INTO posts (
+                name, comment, update_datetime , pass
+            ) VALUES (
+                :name, :comment, :update_datetime, :pass
+            )";
+
+        //実行準備
+        $stmt = $dbh -> prepare($sql);
+        //クエリのパラメータごとに値を組み込む
+        $stmt -> bindValue(':name', $name);
+        $stmt -> bindValue(':comment', $comment);
+        $stmt -> bindValue(':update_datetime', $update_time);
+        $stmt -> bindValue(':pass', $pass);
+        //組み込んだ後にSQL文を実行
+        $stmt -> execute();
+        
+
+    //データの取得
         $stmt = $dbh->query("SELECT * FROM posts");
         $results = $stmt->fetchall(PDO::FETCH_ASSOC);
         echo var_dump($results);
-
-        //データの追加
         
         
 
