68e6225a25e606d35a0d4aaedbf1fc1e7c0f23de
Yuta_Nakjima_Dell <nkjmyut0511@gmail.com>
2022-01-27 14:39:07 +0900

    first commit

--- kadai4/login.php
+++ kadai4/login.php
@@ -25,7 +25,6 @@ $smarty->cache_dir    = '/home/co-19-356.99sv-coco.com/public_html/kadai4/smarty
 
 
 $smarty->assign("title", "ログイン");
-
 $smarty->assign("users", $users);
 
 $smarty->display("login.tpl");
